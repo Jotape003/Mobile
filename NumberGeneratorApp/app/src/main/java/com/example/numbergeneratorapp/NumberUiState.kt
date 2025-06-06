efe726c00a66c201c592723c7c3982f35dd2aed1
Joao Pedro <jpedrosm2704@gmail.com>
2025-06-06 17:05:20 -0300

    NumberGenerator

---
+++ NumberGeneratorApp/app/src/main/java/com/example/numbergeneratorapp/NumberUiState.kt
@@ -0,0 +1,8 @@
+package com.example.numbergeneratorapp;
+
+public open class NumberUiState {
+    object Idle : NumberUiState()
+    object Loading : NumberUiState()
+    data class Success(val number : Int) : NumberUiState()
+    data class Error(val message : String) : NumberUiState()
+}
---
+++ NumberGeneratorApp/app/src/main/java/com/example/numbergeneratorapp/NumberViewModal.kt
@@ -0,0 +1,32 @@
+package com.example.numbergeneratorapp;
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+public class NumberViewModel : ViewModel(){
+    var uiState by mutableStateOf<NumberUiState>(NumberUiState.Idle)
+        private set
+    fun generateNumber(){
+        viewModelScope.launch {
+            uiState = NumberUiState.Loading
+            delay(2000)
+
+//            if (Random.nextBoolean()){
+//                uiState = NumberUiState.Error('Erro ao gerar o número, tente novamente')
+//                return@launch
+//            }
+
+            val randomNumber = (1 .. 100).random()
+            uiState = NumberUiState.Success(randomNumber)
+        }
+    }
+
+    fun reset(){
+        uiState = NumberUiState.Idle
+    }
+}
