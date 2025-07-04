ea05a05ed6332967b32b86a2cb4e08b463f725a6
Joao Pedro <jpedrosm2704@gmail.com>
2025-07-04 14:04:49 -0300

    PostApp feito

---
+++ PostApp2/app/src/main/java/com/example/postapp/data/model/User.kt
@@ -0,0 +1,11 @@
+package com.example.postapp.data.model
+
+data class User (
+    val id: Int,
+    val name: String,
+    val email: String
+)
+data class UserCreateRequest (
+    val name: String,
+    val email: String,
+)
